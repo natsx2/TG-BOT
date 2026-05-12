f3adbb75ac73c916c72f00427136cd34a17f1e5b
natsx2 <vernesg507@gmail.com>
2026-05-12 15:05:26 +0800

    update artifacts/api-server/src/routes/index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,16 +1,16 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import authRouter from "./auth";
-import usersRouter from "./users";
 import botRouter from "./bot";
-import activityRouter from "./activity";
+import logsRouter from "./logs";
+import usersRouter from "./users";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
-router.use(authRouter);
-router.use(usersRouter);
-router.use(botRouter);
-router.use(activityRouter);
+router.use("/auth", authRouter);
+router.use("/bot", botRouter);
+router.use("/logs", logsRouter);
+router.use("/users", usersRouter);
 
 export default router;
